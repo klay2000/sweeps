53af8ee68d8db4fb3a1863c299ea32fb67b2ddcc
klayton smith <motoklay@gmail.com>
2020-01-15 16:50:26 -0500

    push

--- src/main/java/progmmo/server/ServerApplication.java
+++ src/main/java/progmmo/server/ServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import progmmo.server.apikey.APIKey;
 import progmmo.server.apikey.APIKeyRepository;
 
@@ -19,9 +21,10 @@ public class ServerApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.save(new APIKey("bloop"));
-        repository.save(new APIKey("bleep"));
-        repository.save(new APIKey("blop"));
+
+        repository.save(new APIKey("bloop", "123"));
+        repository.save(new APIKey("bleep", "123"));
+        repository.save(new APIKey("blop", "123"));
 
         //System.out.println(repository.findByPrefix("bleep"));
 
