c87439f03ff333567c250e89a75b12a7819c5452
sarenord <ajsheld12@gmail.com>
2020-02-04 09:59:40 -0500

    I don't even really know

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:latest
-ENV WORLD_SEED 110101001101
-COPY target/server-0.0.1-SNAPSHOT.jar server.jar
+WORKDIR /usr/src/progmmo
+COPY ./target/server-0.0.1-SNAPSHOT.jar /usr/src/progmmo/server.jar
 CMD [ "java", "-jar", "server.jar" ]
 EXPOSE 443
--- deploy.py
+++ deploy.py
@@ -1,37 +1,61 @@
+import argparse
 import docker
 client = docker.from_env()
+host_images = [ n.tags[0] for n in client.images.list() ]
+host_containers = [ n.name for n in client.containers.list() ]
 
 
 # make sure host has all necessary containers
 def check_dependencies(deps):
-    host_images = [ n.tags[0] for n in client.images.list() ]
     for i in deps:
         if not(i in host_images):
             print("pulling image: ", i)
             client.images.pull(i)
 
+def clean_containers():
+    for c in host_containers:
+        client.containers.get(c).remove(force=True)
 
-dependencies = ["mongo:latest", "openjdk:latest"]
-check_dependencies(dependencies)
+def clean_images():
+    for i in host_images:
+        client.images.get(i).remove(force=True)
 
-# build the container. This was originally in check_dependencies, but I think that the container should be rebuilt on every deploy
-print("building progmmo container")
-client.images.build(path=".", tag="progmmo:latest")
-print("container built successfully")
 
-# docker will create duplicate networks without protest so this check needs to be here
-networkExistsFlag = False
-for network in client.networks.list():
-    if network.name == "dblan":
-        networkExistsFlag = True
-if networkExistsFlag == False:
-    client.networks.create("dblan", driver="bridge", internal=True)
-    print("creating network")
+def main(containerClean=False, imageClean=False):
+    if containerClean:
+        clean_containers()
+    if imageClean:
+        clean_images()
 
-# assmble everything into networks
-dblanar = list(filter(lambda n: n.name == "dblan", client.networks.list()))
-dblan = dblanar[0]
-mongo = client.containers.run("mongo:latest", detach=True)
-dblan.connect(mongo)
-client.containers.run("progmmo:latest")
-dblan.connect(client.containers.get("progmmo:latest"))
+    dependencies = ["mongo:latest", "openjdk:latest"]
+    check_dependencies(dependencies)
+    # build the container. This was originally in check_dependencies, but I think that the container should be rebuilt on every deploy
+    if not("progmmo:latest" in host_images):
+        print("building progmmo container")
+        client.images.build(path=".", tag="progmmo:latest")
+        print("container built successfully")
+        # docker will create duplicate networks without protest so this check needs to be here
+        networkExistsFlag = False
+        for network in client.networks.list():
+            if network.name == "dblan":
+                networkExistsFlag = True
+        if networkExistsFlag == False:
+            client.networks.create("dblan", driver="bridge", internal=True)
+            print("creating network")
+        # assmble everything into networks
+        dblanar = list(filter(lambda n: n.name == "dblan", client.networks.list()))
+        dblan = dblanar[0]
+        if not("mongo:latest" in host_containers):
+            mongo = client.containers.run("mongo:latest", detach=True)
+        dblan.connect(mongo)
+        progmmo = client.containers.run("progmmo:latest")
+        dblan.connect(client.containers.get("progmmo:latest"))
+        print(progmmo.logs())
+
+
+
+if __name__ =="__main__":
+    parser = argparse.ArgumentParse()
+    parser.add_argument('--image',
+    parser.add_argument('--container',
+    main()
