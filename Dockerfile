e594f8871eb8b1e4c84c81e22bb8a753aa464a44
sarenord <ajsheld12@gmail.com>
2020-02-11 08:39:05 -0500

    commit to fix broken intellij

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:latest
-WORKDIR /usr/src/progmmo
-COPY ./target/server-0.0.1-SNAPSHOT.jar /usr/src/progmmo/server.jar
-CMD [ "java", "-jar", "server.jar" ]
+WORKDIR /
+ADD target/server-0.0.1-SNAPSHOT.jar server.jar
 EXPOSE 443
+CMD java -jar server.jar
--- deploy.py
+++ deploy.py
@@ -1,61 +1,83 @@
+#!/usr/bin/env python
+
 import argparse
 import docker
+import os
+
 client = docker.from_env()
-host_images = [ n.tags[0] for n in client.images.list() ]
-host_containers = [ n.name for n in client.containers.list() ]
+llApiClient = docker.APIClient(base_url='unix://var/run/docker.sock')
+host_images = [ n for n in client.images.list() ]
+host_containers = [ n for n in client.containers.list() ]
+
+
+def init_mongo(ip='172.17.0.2'):
+    container_id = llApiClient.create_container(
+        'mongo',
+        ports=[27017],
+        detach=True,
+        networking_config=llApiClient.create_networking_config({
+            'dblan': llApiClient.create_endpoint_config(ipv4_address='172.17.0.2', aliases=['mongo'])
+        })
+    )
+    return client.containers.get(container_id)
 
 
 # make sure host has all necessary containers
 def check_dependencies(deps):
-    for i in deps:
-        if not(i in host_images):
-            print("pulling image: ", i)
-            client.images.pull(i)
+        for i in deps:
+            if not(i in [ i.tags[0] for i in host_images ]):
+                print("pulling image: ", i)
+                client.images.pull(i)
+
 
 def clean_containers():
+    print("cleaning containers")
     for c in host_containers:
-        client.containers.get(c).remove(force=True)
+        c.remove(force=True)
+
 
 def clean_images():
+    print("cleaning images")
     for i in host_images:
-        client.images.get(i).remove(force=True)
+        client.images.remove(i.id, force=True)
 
 
-def main(containerClean=False, imageClean=False):
-    if containerClean:
-        clean_containers()
-    if imageClean:
-        clean_images()
-
+def main():
     dependencies = ["mongo:latest", "openjdk:latest"]
     check_dependencies(dependencies)
-    # build the container. This was originally in check_dependencies, but I think that the container should be rebuilt on every deploy
     if not("progmmo:latest" in host_images):
         print("building progmmo container")
         client.images.build(path=".", tag="progmmo:latest")
         print("container built successfully")
-        # docker will create duplicate networks without protest so this check needs to be here
-        networkExistsFlag = False
-        for network in client.networks.list():
-            if network.name == "dblan":
-                networkExistsFlag = True
-        if networkExistsFlag == False:
-            client.networks.create("dblan", driver="bridge", internal=True)
-            print("creating network")
-        # assmble everything into networks
-        dblanar = list(filter(lambda n: n.name == "dblan", client.networks.list()))
-        dblan = dblanar[0]
-        if not("mongo:latest" in host_containers):
-            mongo = client.containers.run("mongo:latest", detach=True)
-        dblan.connect(mongo)
-        progmmo = client.containers.run("progmmo:latest")
-        dblan.connect(client.containers.get("progmmo:latest"))
-        print(progmmo.logs())
+
+    # docker will create duplicate networks without protest so this check needs to be here
+    if not("dblan" in [n.name for n in client.networks.list() ]):
+        print("creating network")
+        client.networks.create("dblan", driver="bridge", internal=True)
+
+    # assemble everything into networks
+    dblanar = list(filter(lambda n: n.name == "dblan", client.networks.list()))
+    dblan = dblanar[0]
+
+    if not("mongo:latest" in host_containers):
+        mongo = init_mongo()
+
+    progmmo = client.containers.run("progmmo:latest",
+                                    network=dblan.id,
+                                    ports={'80/tcp':8080})
+    print("launching container")
+    print(progmmo.logs())
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--images', help="clean host images before running", action="store_true")
+    parser.add_argument('--containers', help="clean host containers before running", action="store_true")
+    args = parser.parse_args()
 
-if __name__ =="__main__":
-    parser = argparse.ArgumentParse()
-    parser.add_argument('--image',
-    parser.add_argument('--container',
+    if args.containers:
+        clean_containers()
+
+    if args.images:
+        clean_images()
     main()
