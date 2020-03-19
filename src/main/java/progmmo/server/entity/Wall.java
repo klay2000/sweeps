8ed025281c0db954bcf8c9d95ce7786feb46da2f
sarenord <ajsheld12@gmail.com>
2020-03-18 22:30:34 -0400

    added wall class for loading the map into the database

--- src/main/java/progmmo/server/entity/Wall.java
+++ src/main/java/progmmo/server/entity/Wall.java
@@ -1,4 +1,7 @@
 package progmmo.server.entity;
 
-public class Wall {
+public class Wall extends Entity {
+    public Wall(int x, int y, String sectorID) {
+        super(x, y, sectorID, "SYSTEM", "wall", -1);
+    }
 }
